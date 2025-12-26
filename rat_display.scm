0560fbcb3cea208ff0b32c2c29c990fb0967e858
Heiko Goes <heiko.goes@gaebler-goes.de>
2025-12-26 15:35:17 +0100

    added rational numbers + display

---
+++ rat_display.scm
@@ -0,0 +1,15 @@
+(define (make-rat n d) (cons n d))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x))
+)
+
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+
+(make-rat -4 -2)
--- test.scm
+++
@@ -1,8 +0,0 @@
-(define (fib_iter n actual-n n1 n2)
-  (if (= actual-n n)
-    (+ n1 n2)
-    (fib_iter n (+ actual-n 1) actual-n n1)
-  )
-)
-
-(fib_iter 3 2 1 0)
