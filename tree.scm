4bc15083ead34dc8b498af2f084c82a44dc83e0e
Heiko Goes <heiko.goes@gaebler-goes.de>
2026-01-17 17:11:53 +0100

    use pair? fom book.

--- tree.scm
+++ tree.scm
@@ -1,12 +1,12 @@
-(define my-tree '((1 2) (3 4 1) (8) 9))
+(define my-tree '(200 (1 2) (3 4 1) (8) 9))
 
 my-tree
 
 (define (no-of-leafs tree)
   (cond 
     ((null? tree) 0)
-    ((list? tree) (+ (no-of-leafs (car tree)) (no-of-leafs (cdr tree))))
-    (else 1)
+    ((not (pair? tree)) 1)
+    (else (+ (no-of-leafs (car tree)) (no-of-leafs (cdr tree))))
   )
 )
 
