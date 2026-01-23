f3a15e9d39e8faa252832b2a2f098a3d2e6d21a2
Heiko Goes <heiko.goes@gaebler-goes.de>
2026-01-23 10:59:38 +0100

    different way to build proper and not proper lists

--- list.scm
+++ list.scm
@@ -1,3 +1,9 @@
 (list 1 2 3 4)
 
-(cons (cons (cons (cons 1 2) 3) 4) ´())
+(cons 1 (cons 2 (cons 3 (cons 4 '()))))
+
+(cons (cons (cons (cons 1 2) 3) 4) '())
+
+(cons (cons (cons (cons 1 2) 3) 4) 5)
+
+(cons 1 (cons 2 (cons 3 4)))
