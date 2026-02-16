5074ba04c5f864c57d82cc2dcc39169d0ad0dfd8
Heiko Goes <heiko.goes@gaebler-goes.de>
2026-02-16 17:46:53 +0100

    Message passing form SICP Page 186

--- message_passing.scm
+++ message_passing.scm
@@ -13,3 +13,29 @@
 ;; Usage:
 (area '(3 4) rectangle-handler)  ; => 12
 (area '(5) circle-handler)      ; => 78.5398...
+
+;---------- SICP Page 186 -----------------------------------
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+      ((eq? op 'imag-part) y)
+      ((eq? op 'magnitude) 
+        (sqrt (+ (square x) (square y)))
+      )
+      ((eq? op 'angle) (atan x y))
+      (else (error "unknown op -- MAKE-FROM-REAL-IMAG" op))
+    )
+  )
+  dispatch
+)
+
+; the arg is the dispatch function
+(define (apply-generic op arg)
+  (arg op)
+)
+
+(define z (make-from-real-imag 10 15))
+
+(apply-generic 'real-part z)
+(apply-generic 'magnitude z)
+(apply-generic 'angle z)
