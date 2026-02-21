d84643949b52acac37b31296e5a15b68fb795376
Heiko Goes <heiko.goes@gaebler-goes.de>
2026-02-21 14:40:02 +0100

    fixed new-withdraw

--- balance.scm
+++ balance.scm
@@ -1,4 +1,4 @@
-; scip page 220 (Local State Variables)
+; sicp page 220 (Local State Variables)
 (define balance 100)
 
 (define (withdraw amount)
@@ -9,17 +9,17 @@
 
 (withdraw 75)
 
+;-------------------------------------------------------
 
-(define (new-withdraw amount)
-  (let ((balance 100))
+(define (new-withdraw balance)
     (lambda (amount) 
       (if (>= balance amount) 
         (begin (set! balance (- balance amount)) balance)
         "Insufficient funds"
       )
-    )
   )
-  balance
 )
 
-(new-withdraw 25)
+(define W1 (new-withdraw 125))
+
+(W1 25)
