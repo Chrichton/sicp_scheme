db2e3b8d006bc28882de41854172f25e2cbd276c
Heiko Goes <heiko.goes@gaebler-goes.de>
2026-02-24 12:13:48 +0100

    added make-account

--- balance.scm
+++ balance.scm
@@ -11,15 +11,62 @@
 
 ;-------------------------------------------------------
 
-(define (new-withdraw balance)
+(define new-withdraw
+  (let ((balance 100))
     (lambda (amount) 
       (if (>= balance amount) 
         (begin (set! balance (- balance amount)) balance)
         "Insufficient funds"
       )
+    )
   )
 )
 
-(define W1 (new-withdraw 125))
+; doesn't work, because 
+; (define withdraw (new-withdraw))
+
+; (withdraw 1200)
+
+;-------------------------------------------------------
+
+(define (make-withdraw balance)
+  (lambda (amount) 
+    (if (>= balance amount) 
+      (begin (set! balance (- balance amount)) balance)
+      "Insufficient funds"
+    )
+  )
+)
+
+(define W1 (make-withdraw 125))
 
 (W1 25)
+
+
+(define (make-account balance)
+  (define (withdraw amount) 
+    (if (>= balance amount) 
+      (begin (set! balance (- balance amount)) balance)
+      "Insufficient funds"
+    )
+  )
+
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance
+  )
+
+  (define (dispatch m)
+    (cond 
+      ((eq? m 'withdraw) withdraw)
+      ((eq? m 'deposit) deposit)
+      (else ("error unknown request -- MAKE-Account" m))
+    )
+  )
+  dispatch
+)
+
+(define acc (make-account 100))
+
+((acc 'withdraw) 25)
+((acc 'deposit) 30)
