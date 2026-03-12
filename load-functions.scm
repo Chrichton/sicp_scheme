6de4ee79645c9261536111ee7216cce422ca2507
Heiko Goes <heiko.goes@gaebler-goes.de>
2026-03-12 07:09:35 +0100

    use load to reuse functions

---
+++ load-functions.scm
@@ -0,0 +1,6 @@
+(load "fib.scm")
+(fib 5)
+
+; Attention!!
+; load executes any code inside the file too
+; if you want to use load to reuse functions, you have to put these functions in a separate file.
