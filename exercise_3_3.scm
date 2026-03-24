924b06a93188980c592db6bfb13cdb5881eb4e80
Heiko Goes <heiko.goes@gaebler-goes.de>
2026-03-24 11:18:37 +0100

    solved exercise 3.3 with lookung at the solution

---
+++ exercise_3_3.scm
@@ -0,0 +1,33 @@
+(define (make-account balance secret-password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount)) balance)
+      "Insufficient funds"
+    )
+  )
+
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance
+  )
+
+  (define (dispatch m password)
+    (if (eq? password secret-password)
+      (cond ((eq? m 'withdraw) withdraw)
+        ((eq? m 'deposit) deposit)
+        (else (error "Unknown request: MAKE-ACCOUNT" m))
+      ) 
+      (lambda (x) "Incorrect password")
+    )
+  )
+
+  dispatch
+)
+
+(define account (make-account 100 'pw))
+
+((account 'withdraw 'pw) 20)
+((account 'deposit 'pw) 30)
+
+((account 'withdraw 'pw1) 20)
+((account 'deposit 'pw1) 30)
